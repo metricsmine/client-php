33a13cb642d495d8d33924995e1dee0e04c24daa
Pablo Martinez <pablomartinezfernandez@gmail.com>
2017-08-29 21:26:33 +0200

    +

--- src/HttpClient.php
+++ src/HttpClient.php
@@ -23,17 +23,40 @@ class HttpClient {
         curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, true);
     }
 
-    public function send($options) {
+    public function send($options, $endpoint) {
         if (!$this->curl) {
             return;
         }
 
         $url = 'https://' . $this->config['code'] . '.metricsmine.com/api/'
-            . $this->config['key']['public'] . '/logs'
-            . '/' . $this->config['service']
-            . ($this->config['instance'] ? '/' . $this->config['instance'] : '')
-//            . '/'
-        ;
+            . $this->config['key']['public'] . $endpoint;
+
+        $fields = [];
+
+        switch ($endpoint) {
+            case '/logs':
+                $url .= '/' . $this->config['service']
+                    . ($this->config['instance'] ? '/' . $this->config['instance'] : '');
+
+
+                $fields = [
+//            'service' => $options['service'],
+//            'instance' => $options['instance'],
+                    'type'       => $options['type'],
+                    'format'     => $options['format'],
+                    'title'      => (!empty($options['title']) ? (string) $options['title'] : ''),
+                    'message'    => (!empty($options['message']) && is_scalar($options['message'])) ? (string) $options['message'] : json_encode($options['message']),
+                    'stacktrace' => empty($options['stacktrace']) ? null : json_encode($options['stacktrace']),
+                    'file'       => $options['file'],
+                    'line'       => $options['line'],
+                    'url'        => $options['url'],
+                ];
+                break;
+            case '/metrics':
+//                $url .= '/' . $this->config['service']
+//                    . ($this->config['instance'] ? '/' . $this->config['instance'] : '');
+                break;
+        }
 
         curl_setopt($this->curl, CURLOPT_URL, $url);
         curl_setopt($this->curl, CURLOPT_HTTPHEADER, [
@@ -41,18 +64,7 @@ class HttpClient {
             'Content-type: application/x-www-form-urlencoded',
         ]);
 
-        curl_setopt($this->curl, CURLOPT_POSTFIELDS, http_build_query([
-//            'service' => $options['service'],
-//            'instance' => $options['instance'],
-            'type'       => $options['type'],
-            'format'     => $options['format'],
-            'title'      => (!empty($options['title']) ? (string) $options['title'] : ''),
-            'message'    => (!empty($options['message']) && is_scalar($options['message'])) ? (string) $options['message'] : json_encode($options['message']),
-            'stacktrace' => empty($options['stacktrace']) ? null : json_encode($options['stacktrace']),
-            'file'       => $options['file'],
-            'line'       => $options['line'],
-            'url'        => $options['url'],
-        ]));
+        curl_setopt($this->curl, CURLOPT_POSTFIELDS, http_build_query($fields));
 
         curl_exec($this->curl);
     }
